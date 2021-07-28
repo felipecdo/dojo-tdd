cf9a2dc09c7cd5f72e9f23048285ca5b37df8a79
Felipe C <felipe.oliveira@resultadosdigitais.com.br>
2021-07-28 13:05:49 -0300

    dojo 2 hours result

--- app/roman/roman.rb
+++ app/roman/roman.rb
@@ -1,5 +1,27 @@
 class Roman
-    def convert(var)
-        var + 1
+    def convert(arabic)
+
+        if (arabic > 10 && arabic < 50)
+            return "X#{convert(arabic-10)}"
+        end
+
+        if (arabic > 5 && arabic < 9)
+            return "#{convert(arabic-1)}I"
+        end
+
+        if ((arabic % 10) == 9)
+            return "I#{convert(arabic+1)}"
+        end
+
+        arabic_to_romans = {
+            '1'=> 'I',
+            '2'=> 'II',
+            '3'=> 'III',
+            '4'=> 'IV',
+            '5'=> 'V',
+            '10'=>'X',
+            '50'=>'L'
+        }
+        arabic_to_romans[arabic.to_s]
     end
 end
--- spec/roman/roman_spec.rb
+++ spec/roman/roman_spec.rb
@@ -1,9 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Roman do
-  describe '.convert' do
-    it 'sums 1' do
-      expect(Roman.new.convert(1)).to be(2)
+  shared_examples 'should convert' do |n, r|
+    it 'khjs' do
+      expect(Roman.new.convert(n)).to eq(r)
     end
+  end
+
+  describe '.convert' do
+    include_examples 'should convert', 1, 'I'
+
+    include_examples 'should convert', 2, 'II'
+
+    include_examples 'should convert', 3, 'III'
+
+    include_examples 'should convert', 4, 'IV'
+
+    include_examples 'should convert', 5, 'V'
+
+    include_examples 'should convert', 6, 'VI'
+
+    include_examples 'should convert', 7, 'VII'
+
+    include_examples 'should convert', 8, 'VIII'
+
+    include_examples 'should convert', 9, 'IX'
+
+    include_examples 'should convert', 10, 'X'
+
+    include_examples 'should convert', 19, 'XIX'
+
+    include_examples 'should convert', 20, 'XX'
+
+    include_examples 'should convert', 25, 'XXV'
+
+    include_examples 'should convert', 29, 'XXIX'
+
+    include_examples 'should convert', 50, 'L'
+
   end
 end
